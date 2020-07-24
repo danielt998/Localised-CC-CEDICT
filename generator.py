12bf467989d4b8c4bdd5653696c510793beda04b
danielt998 <daniel@fearnley.net>
2020-07-24 16:35:20 +0100

    move to json for replacement data

--- generator.py
+++ generator.py
@@ -1,16 +1,17 @@
-import replacements.en_GB as engb
+import json
 
 source_cedict = open('cedict_ts.u8', encoding='utf-8')
 output_cedict = open('cedict_en-gb.u8', 'w', encoding='utf-8')
 
-line_replacements = engb.getReplacements()
+with open('replacements/replacements.json', encoding='utf-8') as file:
+    line_replacements = json.load(file)
 
 for line in source_cedict:
     line = line.rstrip('\n')
     replaced = False
-    for replacement_pair in line_replacements:
-        if(line == replacement_pair[0]):
-            output_cedict.write(replacement_pair[1] + '\n')
+    for replacement_set in line_replacements['data']:
+        if(line == replacement_set['original']):
+            output_cedict.write(replacement_set['en-GB'] + '\n')
             replaced = True
     if not replaced:
         output_cedict.write(line + '\n')
